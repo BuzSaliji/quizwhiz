f1f0799ed5a9823d7112e73b30bbefb910c7fa1c
Burim Saliji <buz.saliji@gmail.com>
2023-10-11 20:06:39 +1100

    implement categories routing and set up quiz page

---
+++ src/components/Home.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const categories = [
+  { name: 'General Knowledge', value: '9' },
+  { name: 'Film', value: '11' },
+  { name: 'Music', value: '12' },
+  { name: 'Sports', value: '21' },
+  { name: 'History', value: '23' },
+];
+
+const Home = () => {
+  return (
+    <div>
+      <h1>Welcome to QuizWhiz</h1>
+      <p>Select a category to start the quiz:</p>
+      <ul>
+        {categories.map((category) => (
+          <li key={category.value}>
+            <Link to={`/quiz/${category.value}`}>{category.name}</Link>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default Home;
---
+++ src/components/Quiz.jsx
@@ -0,0 +1,37 @@
+import React, { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
+
+const Quiz = () => {
+  const { categoryId } = useParams();
+  const [questions, setQuestions] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchQuestions = async () => {
+      setLoading(true);
+
+        const response = await fetch(`https://opentdb.com/api.php?amount=10&category=${categoryId}`);
+        const data = await response.json();
+        setQuestions(data.results);
+    
+        setLoading(false);
+      };
+
+    fetchQuestions();
+  }, [categoryId]);
+
+  if (loading) return <p>Loading...</p>;
+
+  return (
+    <div>
+      <h1>Quiz</h1>
+      {questions.map((question, index) => (
+        <div key={index}>
+          <p dangerouslySetInnerHTML={{ __html: question.question }} />
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default Quiz;
