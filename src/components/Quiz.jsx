ca248adc71648975a873b5c90c375bc5fdfe8976
Burim Saliji <buz.saliji@gmail.com>
2023-10-13 20:31:11 +1100

    implement context API for global state

--- src/components/Quiz.jsx
+++ src/components/Quiz.jsx
@@ -7,19 +7,14 @@ const Quiz = () => {
   const [questions, setQuestions] = useState([]);
   const [loading, setLoading] = useState(true);
   const [currentQuestionIndex, setCurrentQuestionIndex] = useState(0);
-  const [selectedAnswer, setSelectedAnswer] = useState(null);
   const [score, setScore] = useState(0);
   const navigate = useNavigate();
   const [isQuizFinished, setIsQuizFinished] = useState(false);
 
-
   const playAgain = () => {
-    // Reset any quiz state if needed
     setCurrentQuestionIndex(0);
     setScore(0);
-    setSelectedAnswer(null);
-
-    // Navigate to the home page
+    setIsQuizFinished(false);
     navigate('/');
   };
 
@@ -36,70 +31,49 @@ const Quiz = () => {
         setLoading(false);
       }
     };
-  
     fetchQuestions();
   }, [categoryId]);
-  
-
-  if (loading) return <p>Loading...</p>;
-
-  // eslint-disable-next-line no-unused-vars
-  const handleNextQuestion = () => {
-    if (selectedAnswer === questions[currentQuestionIndex].correct_answer) {
-      setScore((prevScore) => prevScore + 1);
-    }
-    if (currentQuestionIndex < questions.length - 1) {
-      setCurrentQuestionIndex((prevIndex) => prevIndex + 1);
-      setSelectedAnswer(null); 
-    } else {
-      alert(`Quiz finished! Your score: ${score}`);
-    }
-  };
 
   const handleAnswerClick = (answer) => {
-    setSelectedAnswer(answer);
-    console.log("Selected Answer:", answer);
-  
     if (answer === questions[currentQuestionIndex].correct_answer) {
       setScore((prevScore) => prevScore + 1);
     }
-  
-    
-    setTimeout(() => {
-      if (currentQuestionIndex < questions.length - 1) {
-        setCurrentQuestionIndex((prevIndex) => prevIndex + 1);
-        setSelectedAnswer(null);
-      } else {
-        setIsQuizFinished(true);
-      }
-    }, 500);
+
+    if (currentQuestionIndex < questions.length - 1) {
+      setCurrentQuestionIndex((prevIndex) => prevIndex + 1);
+    } else {
+      setIsQuizFinished(true);
+    }
   };
 
-  const currentQuestion = questions[currentQuestionIndex];
-  const allAnswers = [...currentQuestion.incorrect_answers, currentQuestion.correct_answer];
-  const shuffledAnswers = allAnswers.sort(() => Math.random() - 0.5);
+  let shuffledAnswers = [];
+  if (!loading && questions[currentQuestionIndex]) {
+    const currentQuestion = questions[currentQuestionIndex];
+    shuffledAnswers = [...currentQuestion.incorrect_answers, currentQuestion.correct_answer].sort(() => Math.random() - 0.5);
+  }
 
   return (
     <div className="quiz">
       <h1>Quiz</h1>
-      {!isQuizFinished ? (
+      {!loading && questions[currentQuestionIndex] && !isQuizFinished ? (
         <div className="container">
-          <p dangerouslySetInnerHTML={{ __html: questions[currentQuestionIndex]?.question }} />
+          <p dangerouslySetInnerHTML={{ __html: questions[currentQuestionIndex].question }} />
           {shuffledAnswers.map((answer, index) => (
             <button key={index} onClick={() => handleAnswerClick(answer)}>
               {answer}
             </button>
           ))}
         </div>
-      ) : (
+      ) : null}
+      {isQuizFinished && (
         <div className='endgame'>
           <h1 className='endheader'>Your Score: {score}</h1>
           <button onClick={playAgain}>Play Again</button>
         </div>
       )}
+      {loading && <p>Loading...</p>}
     </div>
   );
-  
 }
 
 export default Quiz;
